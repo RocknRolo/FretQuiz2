aa57787062f242563ea8aca794a6076bed82b566
Roel Kemp <42418205+RocknRolo@users.noreply.github.com>
2022-09-22 16:56:54 +0200

    Added display stuff. Added better questioning.
    The tone that you hover over with your mouse will now be displayed all over the fretboard. Questions are now restricted to 12 tones and will never repeat before all other tones have been questioned. Fixed some other minor stuff, but I'm not entirely satisfied yet.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // Auteur: Roel Kemp
 
-const canvas = document.getElementById('canvas');
-canvas.setAttribute("width", window.innerWidth+"");
-canvas.setAttribute("height", "330");
-canvas.addEventListener("mouseup", function(e) {
-    readAnswer(canvas, e);
-});
-const ctx = canvas.getContext('2d');
+const canvas1 = document.getElementById('canvas1');
+canvas1.setAttribute("width", window.innerWidth+"");
+canvas1.setAttribute("height", "300");
 
-const scaleDisplay = document.getElementById("fretboard");
-scaleDisplay.style.fontSize = 1 + (innerWidth / 1000) + "rem";
+const canvas2 = document.getElementById('canvas2');
+canvas2.setAttribute("width", window.innerWidth+"");
+canvas2.setAttribute("height", "300");
+canvas2.addEventListener("mousemove", function(e) {
+    updateLabels(canvas2, e);
+});
+canvas2.addEventListener("mouseup", function(e) {
+    readAnswer(canvas2, e);
+});
+const ctx1 = canvas1.getContext('2d');
+const ctx2 = canvas2.getContext('2d');
+
 
 // Hier kunnen snaren worden toegevoegd of worden verwijderd.
 // "flatSharp" geeft aan of een toon verhoogd of verlaagd is.
@@ -61,6 +67,7 @@ const FRET_Ys = calcFretYs();
 const LABEL_SIZE = innerWidth / 220 + 10;
 const LABEL_COLOR = "#F00" // Rood
 const LABEL_COLOR_OPEN = "#111" // Donkergrijs
+const LABEL_COLOR_SEL = "#FF0" // Geel
 const LABEL_TEXT_FONT = (innerWidth / 1500) + 1 + "rem courier new";
 const LABEL_TEXT_COLOR = "#FFF";
 const LABEL_TEXT_X_OFFSET = -13;
@@ -89,8 +96,7 @@ function calcToneXs() {
 function calcStringYs() {
     let stringYs = [];
     for (let i = 0; i < TUNING.length; i++) {
-        let stringY = FRETBOARD_Y + STRING_EDGE_SPACE + (i * ((FRETBOARD_HEIGHT - STRING_EDGE_SPACE * 2) / 
-            (TUNING.length - 1))) - i;
+        let stringY = FRETBOARD_Y + STRING_EDGE_SPACE + (i * ((FRETBOARD_HEIGHT - STRING_EDGE_SPACE * 2) / (TUNING.length - 1))) - i;
         stringYs.push(stringY);
     }
     return stringYs;
@@ -109,75 +115,86 @@ function calcFretYs() {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function drawFretboard() {
-    ctx.beginPath();
+    ctx1.beginPath();
     // Teken de toets.
-    ctx.fillStyle = FRETBOARD_COLOR;
-    ctx.fillRect(FRETBOARD_X, FRETBOARD_Y, FRETBOARD_WIDTH, FRETBOARD_HEIGHT);
+    ctx1.fillStyle = FRETBOARD_COLOR;
+    ctx1.fillRect(FRETBOARD_X, FRETBOARD_Y, FRETBOARD_WIDTH, FRETBOARD_HEIGHT);
 
     // Teken de frets.
-    ctx.fillStyle = FRET_COLOR;
+    ctx1.fillStyle = FRET_COLOR;
     for (let i = 0; i < calcFretXs().length; i++) {
-        ctx.fillRect(FRET_Xs[i], FRETBOARD_Y, FRET_SIZE, FRETBOARD_HEIGHT);
+        ctx1.fillRect(FRET_Xs[i], FRETBOARD_Y, FRET_SIZE, FRETBOARD_HEIGHT);
     }
 
     // Teken de inlays en de positie nummers.
-    ctx.font = POS_NR_FONT;
+    ctx1.font = POS_NR_FONT;
     for (let i = 0; i < INLAY_POSITIONS.length; i++) {
-        ctx.fillStyle = INLAY_COLOR;
+        ctx1.fillStyle = INLAY_COLOR;
         if (INLAY_POSITIONS[i] === OCTAVE_FRET || INLAY_POSITIONS[i] === SEC_OCT_FRET) {
-            ctx.beginPath();
-            ctx.arc(TONE_Xs[INLAY_POSITIONS[i]], FRETBOARD_Y + (FRETBOARD_HEIGHT / 6 * 2), INLAY_SIZE, 0, 2 * Math.PI);
-            ctx.fill();
-            ctx.beginPath();
-            ctx.arc(TONE_Xs[INLAY_POSITIONS[i]], FRETBOARD_Y + (FRETBOARD_HEIGHT / 6 * 4), INLAY_SIZE, 0, 2 * Math.PI);
-            ctx.fill();
+            ctx1.beginPath();
+            ctx1.arc(TONE_Xs[INLAY_POSITIONS[i]], FRETBOARD_Y + (FRETBOARD_HEIGHT / 6 * 2), INLAY_SIZE, 0, 2 * Math.PI);
+            ctx1.fill();
+            ctx1.beginPath();
+            ctx1.arc(TONE_Xs[INLAY_POSITIONS[i]], FRETBOARD_Y + (FRETBOARD_HEIGHT / 6 * 4), INLAY_SIZE, 0, 2 * Math.PI);
+            ctx1.fill();
         } else {
-            ctx.beginPath();
-            ctx.arc(TONE_Xs[INLAY_POSITIONS[i]], FRETBOARD_Y + FRETBOARD_HEIGHT / 2, INLAY_SIZE,
+            ctx1.beginPath();
+            ctx1.arc(TONE_Xs[INLAY_POSITIONS[i]], FRETBOARD_Y + FRETBOARD_HEIGHT / 2, INLAY_SIZE,
                 0, 2 * Math.PI);
-            ctx.fill();
+            ctx1.fill();
         }
-        ctx.fillStyle = INLAY_TEXT_COLOR;
-        ctx.fillText(""+INLAY_POSITIONS[i],
+        ctx1.fillStyle = INLAY_TEXT_COLOR;
+        ctx1.fillText(""+INLAY_POSITIONS[i],
             TONE_Xs[INLAY_POSITIONS[i]] + POS_NR_OFFSET + (INLAY_POSITIONS[i] > 9 ? TWO_DIGIT_OFFSET : 0),
             FRETBOARD_Y + FRETBOARD_HEIGHT + FB_NR_SPACING);
     }
 
     // Teken de snaren.
-    ctx.fillStyle = STRING_COLOR;
+    ctx1.fillStyle = STRING_COLOR;
     for (let i = 0; i < STRING_Ys.length; i++) {
-        ctx.fillRect(0, STRING_Ys[i], FRETBOARD_X + FRETBOARD_WIDTH,i + 1);
+        ctx1.fillRect(0, STRING_Ys[i], FRETBOARD_X + FRETBOARD_WIDTH,i + 1);
     }
     // Teken de labels voor de open snaren.
-    ctx.fillStyle = LABEL_COLOR_OPEN;
+    ctx2.fillStyle = LABEL_COLOR_OPEN;
     for (let i = 0; i < STRING_Ys.length; i++) {
-        drawToneTextLabel(0, i, TUNING[i].toString());
+        drawToneTextLabel(ctx1, 0, i, TUNING[i].toString());
     }
 }
 
-function drawToneLabel (fingerPos, stringNum) {
+function drawToneLabel (ctx, fingerPos, stringNum) {
     ctx.beginPath();
     ctx.arc(TONE_Xs[fingerPos], STRING_Ys[stringNum], LABEL_SIZE, 0, 2 * Math.PI);
     ctx.fill();
 }
 
-function drawToneTextLabel (fingerPos, stringNum, text) {
+function drawToneTextLabel (ctx, fingerPos, stringNum, text) {
     ctx.fillStyle = LABEL_COLOR_OPEN;
-    drawToneLabel(fingerPos, stringNum);
+    drawToneLabel(ctx, fingerPos, stringNum);
     ctx.fillStyle = LABEL_TEXT_COLOR;
     ctx.beginPath();
     ctx.fillText(text, TONE_Xs[fingerPos] + LABEL_TEXT_X_OFFSET, STRING_Ys[stringNum] + LABEL_TEXT_Y_OFFSET, LABEL_SIZE * 1.5);
 }
 
-function reDrawFretboard() {
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    drawFretboard();
-    ctx.fillStyle = LABEL_COLOR;
-}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const SEMITONES = "C D EF G A B";
 const FLATTONES = "ABDE";
+const QUESTONES = [
+    new Tone("A", -1),
+    new Tone("A", 0),
+    new Tone("B", -1),
+    new Tone("B", 0),
+    new Tone("C", 0),
+    new Tone("C", 1),
+    new Tone("D", 0),
+    new Tone("E", -1),
+    new Tone("E", 0),
+    new Tone("F", 0),
+    new Tone("F", 1),
+    new Tone("G", 0)
+]
+
+let questones = [];
 
 let correctTone;
 let answerTone;
@@ -198,6 +215,11 @@ function Tone(natural, flatSharp) {
     }
 }
 
+function PosIndex(stringIndex, fretIndex) {
+    this.stringIndex = stringIndex;
+    this.fretIndex = fretIndex;
+}
+
 // Deze functie ontvangt 2 Tone objecten en geeft het aantal halve tonen ertussen terug.
 // Het eerste Tone object wordt altijd als lager gezien dan het 2de Tone object.
 // Het getal dat je terugkrijgt als je vergelijkt met "C, 0" correspondeert aan de antwoordknoppen in de GUI.
@@ -207,9 +229,13 @@ function semitonesBetween(tone1, tone2) {
     return (Math.max(index1, index2) - Math.min(index1, index2)) % 12;
 }
 
-
 function question() {
-    correctTone = new Tone(String.fromCharCode(65 + Math.floor(Math.random() * 7)), -1 + Math.floor(Math.random() * 3));
+    if (questones.length === 0) {
+        questones = [...QUESTONES];
+    }
+    let randex = Math.floor(Math.random() * questones.length);
+    correctTone = questones[randex];
+    questones.splice(randex, 1);
     document.getElementById('question').textContent = "Where is: " + correctTone + " ?";
 }
 
@@ -229,16 +255,41 @@ function updateScore() {
     document.getElementById('mistakes').textContent = mistakes;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let posIndex;
+
+function updateLabels(canvas, event) {
+    ctx2.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    ctx2.fillStyle = LABEL_COLOR_SEL;
+
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+
+    posIndex = posToIndex(x, y);
+
+    if (posIndex != null && posIndex.stringIndex != undefined) {
+        for (let i = 0; i < TUNING.length; i++) {
+            for (let j = 0; j < TONE_Xs.length; j++) {
+                let loopTone = new Tone(TUNING[i], j);
+                let hoverTone = new Tone(TUNING[posIndex.stringIndex].natural, posIndex.fretIndex);
+
+                if (semitonesBetween(loopTone, hoverTone) === 0) {
+                    drawToneLabel(ctx2, j, i);
+                }
+            }
+        }
+    }
+}
 
 function readAnswer(canvas, event) {
     let rect = canvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
-    answerTone = posToTone(x, y);
+    answerTone = new Tone(TUNING[posToIndex(x, y).stringIndex], posToIndex(x, y).fretIndex);
     checkAnswer();
 }
 
-function posToTone(x, y) {
+function posToIndex(x, y) {
     if (x > FRET_Xs[FRET_Xs.length - 1] || y > FRET_Ys[FRET_Ys.length - 1]) {
         return null;
     }
@@ -246,26 +297,23 @@ function posToTone(x, y) {
     let fretXs = [...FRET_Xs];
     fretXs.unshift(0);
 
-    let fretNum;
+    let fretIndex;
     for (let i = 0; i < fretXs.length - 1; i++) {
         if (x > fretXs[i] && x < fretXs[i + 1]) {
-            fretNum = i;
+            fretIndex = i;
         }
     }
 
-    let stringChar;
+    let stringIndex;
     for (let i = 0; i < FRET_Ys.length - 1; i++) {
         if (y > FRET_Ys[i] && y < FRET_Ys[i + 1]) {
-            stringChar = TUNING[i].natural;
+            stringIndex = i;
         }
     } 
 
-    //console.log(stringChar + " " + fretNum);
-    return new Tone(stringChar, fretNum);
+    return new PosIndex(stringIndex, fretIndex);
 }
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 drawFretboard();
 question();
-
